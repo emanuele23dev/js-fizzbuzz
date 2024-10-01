84fc836373f31b01132d213a091cecd82f28701e
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-01 14:25:41 +0200

    raccolta dati da 1 a 100

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('ciao');
+// Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. 
+
+// strumenti conosciuti : 
+// -let
+// -console log 
+// -operatori aritmetici e di comparazione relazionali e logici
+// -string 
+// -number
+// -conditionals if/elseif/else
+// -ciclo for
+
+// 1 preparazione 
+// creo la struttura del ciclo for
+
+// for () {
+
+// }
+
+// 2 raccolta Dati 
+// nel ciclo for definisco la variabile i e loggo in per assicurarmi che non ci siano errori in console
+
+for ( let i = 1; i<=100; i++) {
+console.log(i);
+}
